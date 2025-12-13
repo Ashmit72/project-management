ded5c112dc09705efc69d6d07c537657b3d33390
Keshav Kishor Ram <ramabishek40@gmail.com>
2025-12-13 09:24:42 +0545

    feat: show  board updates in realtime

--- src/providers/ProjectProvider.tsx
+++ src/providers/ProjectProvider.tsx
@@ -1,18 +1,28 @@
 import useSocket from '@/hooks/useSocket';
-import { useEffect } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+import { useCallback, useEffect } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 
 export default function ProjectProvider() {
   const { projectId } = useParams();
   const { socket, connected } = useSocket();
+  const queryClient = useQueryClient();
+
+  const handleBoardUpdate = useCallback(async () => {
+    queryClient.invalidateQueries({
+      queryKey: ['projects', projectId, 'board'],
+    });
+  }, [projectId]);
 
   useEffect(() => {
     if (!connected || !projectId) return;
     socket.emitWithAck('project:subscribe', { projectId });
+    socket.on('board:update', handleBoardUpdate);
 
     return () => {
       socket.emitWithAck('project:unsubscribe', { projectId });
+      socket.off('board:update', handleBoardUpdate);
     };
-  }, [connected, projectId]);
+  }, [connected, projectId, handleBoardUpdate]);
   return <Outlet />;
 }
